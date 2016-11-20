80d5544f59f49dd00d8aea28cbac5ebe8311ecdd
AlexKhram <akhramenkov@gmail.com>
2016-11-20 21:26:44 +0300

    Add table.sql

--- tables.sql
+++ tables.sql
@@ -1,3 +1,141 @@
-CREATE TABLE `rss`.`chanels` ( `id` INT NOT NULL AUTO_INCREMENT , `url` VARCHAR(40) NOT NULL , PRIMARY KEY (`id`), UNIQUE (`url`)) ENGINE = InnoDB;
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.7
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Ноя 20 2016 г., 21:24
+-- Версия сервера: 5.5.50
+-- Версия PHP: 5.6.23
 
-CREATE TABLE `rss`.`user_chanels` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `chanel_id` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+--
+-- База данных: `rss`
+--
+CREATE DATABASE IF NOT EXISTS `rss` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `rss`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `channels`
+--
+
+CREATE TABLE IF NOT EXISTS `channels` (
+  `id` int(11) NOT NULL,
+  `url` varchar(120) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `feeds`
+--
+
+CREATE TABLE IF NOT EXISTS `feeds` (
+  `id` int(11) NOT NULL,
+  `chanel_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `link` varchar(500) NOT NULL,
+  `guid` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=357 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_channels`
+--
+
+CREATE TABLE IF NOT EXISTS `user_channels` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `chanel_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `channels`
+--
+ALTER TABLE `channels`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url` (`url`);
+
+--
+-- Индексы таблицы `feeds`
+--
+ALTER TABLE `feeds`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `guid_3` (`guid`),
+  ADD KEY `chanel_id` (`chanel_id`),
+  ADD KEY `guid` (`guid`),
+  ADD KEY `guid_2` (`guid`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Индексы таблицы `user_channels`
+--
+ALTER TABLE `user_channels`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `chanel_id` (`chanel_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `channels`
+--
+ALTER TABLE `channels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT для таблицы `feeds`
+--
+ALTER TABLE `feeds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=357;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT для таблицы `user_channels`
+--
+ALTER TABLE `user_channels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=47;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `user_channels`
+--
+ALTER TABLE `user_channels`
+  ADD CONSTRAINT `user_channels_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_channels_ibfk_2` FOREIGN KEY (`chanel_id`) REFERENCES `channels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
